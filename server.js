79156a2e8db8fda5d86832bc4eb6a69f91d00fbd
Andrew <andrewrandolph98@gmail.com>
2022-12-19 02:01:46 -0600

    Structured json file

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({extended: false}));
-app.use(express.join());
+app.use(express.json());
 
 const db = mysql.createConnection({
     host: 'localhost',
